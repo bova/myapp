0e05a22e100b7910264a43021c3eae6b85f801c0
Vladimir Povetkin <povetkin84@gmail.com>
2024-07-03 18:38:35 +0500

    Random generator app v1

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+import string
+import random
 
 app = Flask(__name__)
 
-@app.route("/")
+
+def pw_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+@app.route("/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        pw_size = int(request.form["pw_size"])
+        pw = pw_generator(size=pw_size)
+        return render_template("pwgen.html", password=pw)
+
+    return render_template("main.html")
 
 if __name__ == '__main__':
     app.run()
